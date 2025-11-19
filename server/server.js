093039cf6a56531466834ac604ee7cdec764a97d
2225217 <2225217@slu.edu.ph>
2025-11-20 00:57:47 +0800

    6th

--- server/server.js
+++ server/server.js
@@ -120,7 +120,7 @@ app.use('/summary', require('./routes/client/summaryRoutes'));
 //Serve frontend build
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
-  app.get('/path(*)', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
   });
 }
