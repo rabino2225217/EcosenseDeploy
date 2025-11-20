0d0196a0ad2f7042c8a7920cbd8bfc0ad7664801
2225217 <2225217@slu.edu.ph>
2025-11-20 19:22:37 +0800

    change title from "An AI-Powered System for Detecting Trees and Classifying Croplands" to "An AI-Powered System for Detecting Trees and Crop Plots" login page. used 60% model for trees

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -109,7 +109,7 @@ export default function Login() {
           </div>
         </div>
         <p className="absolute bottom-6 left-6 right-6 z-10 text-center text-white text-sm">
-          An AI-Powered System for Detecting Trees and Classifying Croplands
+          An AI-Powered System for Detecting Trees and Crop Plots
           from Drone Imagery for the{" "}
           <b>DENR - Cordillera Administrative Region</b>
         </p>
